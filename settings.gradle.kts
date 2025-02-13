27d613c4375a3bd8eeb907e8def68b5de8f0c445
bhatvaishnavis9 <125575362+bhatvaishnavis9@users.noreply.github.com>
2025-02-13 08:24:52 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+rootProject.name = "Headache_Classification"
+include(":app")
